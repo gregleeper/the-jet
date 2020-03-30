0e26783a8e6c212bf941c27d98960b327dc79066
Greg Leeper <gregleeper@Gregs-MacBook-Pro.local>
2020-03-30 08:31:18 -0500

    font family set

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Header from "./header";
 import Footer from "./footer";
 const Layout = props => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-sans">
       <Header menus={props.menus} />
       <main className="flex-1 max-w-screen mx-auto md:px-0 w-full">
         {props.children}
